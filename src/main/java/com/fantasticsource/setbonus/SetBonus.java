4f9ed060c4417a946853e669b7611efd2130ece6
Kyle Koder <silvertallon@gmail.com>
2019-03-08 06:59:54 -0800

    Initial commit

---
+++ src/main/java/com/fantasticsource/setbonus/SetBonus.java
@@ -0,0 +1,11 @@
+package com.fantasticsource.setbonus;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = SetBonus.MODID, name = SetBonus.NAME, version = SetBonus.VERSION)
+public class SetBonus
+{
+    public static final String MODID = "setbonus";
+    public static final String NAME = "Set Bonus";
+    public static final String VERSION = "1.12.2.000";
+}
