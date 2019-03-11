759f8ba5c344508d70de08b6d607fd47df2cdd81
Kyle Koder <silvertallon@gmail.com>
2019-03-11 01:03:03 -0700

    In theory, attribute modifiers are initialized now

--- src/main/java/com/fantasticsource/setbonus/server/Data.java
+++ src/main/java/com/fantasticsource/setbonus/server/Data.java
@@ -1,5 +1,6 @@
 package com.fantasticsource.setbonus.server;
 
+import com.fantasticsource.mctools.attributes.AttributeMods;
 import com.fantasticsource.mctools.items.ItemFilter;
 import com.fantasticsource.mctools.potions.Potions;
 
@@ -64,7 +65,38 @@ public class Data
         //Initialize attribute modifiers
         for (String string : serverSettings.attributeMods)
         {
-            //TODO
+            String[] tokens = string.split(",");
+            if (tokens.length < 3)
+            {
+                System.err.println("Not enough arguments for attribute bonus: " + string + "\r\nPlease see the examples by hovering the mouse over the config option in the mod config menu");
+                continue;
+            }
+
+            SetData set = sets.get(tokens[0].trim());
+            if (set == null)
+            {
+                System.err.println("Set ID not found (" + tokens[0].trim() + ") for attribute bonus: " + string + "\r\nPlease see the examples by hovering the mouse over the config option in the mod config menu");
+                continue;
+            }
+
+            int numRequired;
+            if (tokens[1].trim().toLowerCase().equals("all")) numRequired = set.getMaxNumber();
+            else numRequired = Integer.parseInt(tokens[1].trim());
+
+            if (numRequired < 1)
+            {
+                System.err.println("Invalid number of set items required (" + tokens[1].trim() + ") for attribute bonus: " + string + "\r\nPlease see the examples by hovering the mouse over the config option in the mod config menu");
+                continue;
+            }
+
+            BonusData data = set.bonuses.get(numRequired);
+            if (data == null)
+            {
+                data = new BonusData();
+                set.bonuses.put(numRequired, data);
+            }
+
+            data.modifiers.addAll(AttributeMods.parseMods(Arrays.copyOfRange(tokens, 2, tokens.length)));
         }
 
 
@@ -81,7 +113,7 @@ public class Data
             SetData set = sets.get(tokens[0].trim());
             if (set == null)
             {
-                System.err.println("Set ID not found (" + tokens[0].trim() + ") for set bonus: " + string + "\r\nPlease see the examples by hovering the mouse over the config option in the mod config menu");
+                System.err.println("Set ID not found (" + tokens[0].trim() + ") for potion bonus: " + string + "\r\nPlease see the examples by hovering the mouse over the config option in the mod config menu");
                 continue;
             }
 
@@ -91,7 +123,7 @@ public class Data
 
             if (numRequired < 1)
             {
-                System.err.println("Invalid number of set items required (" + tokens[1].trim() + ") for set bonus: " + string + "\r\nPlease see the examples by hovering the mouse over the config option in the mod config menu");
+                System.err.println("Invalid number of set items required (" + tokens[1].trim() + ") for potion bonus: " + string + "\r\nPlease see the examples by hovering the mouse over the config option in the mod config menu");
                 continue;
             }
 
@@ -101,7 +133,8 @@ public class Data
                 data = new BonusData();
                 set.bonuses.put(numRequired, data);
             }
-            data.potions = Potions.parsePotions(Arrays.copyOfRange(tokens, 2, tokens.length), true);
+
+            data.potions.addAll(Potions.parsePotions(Arrays.copyOfRange(tokens, 2, tokens.length), true));
         }
     }
 }
