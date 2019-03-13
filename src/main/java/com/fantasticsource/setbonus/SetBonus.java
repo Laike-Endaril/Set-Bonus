d8eae8a4a81d26b794187475665c93741a5004ef
Kyle Koder <silvertallon@gmail.com>
2019-03-13 03:48:48 -0700

    1.12.2.002

--- src/main/java/com/fantasticsource/setbonus/SetBonus.java
+++ src/main/java/com/fantasticsource/setbonus/SetBonus.java
@@ -24,7 +24,7 @@ public class SetBonus
 {
     public static final String MODID = "setbonus";
     public static final String NAME = "Set Bonus";
-    public static final String VERSION = "1.12.2.001";
+    public static final String VERSION = "1.12.2.002";
 
     @EventHandler
     public static void preInit(FMLPreInitializationEvent event)
