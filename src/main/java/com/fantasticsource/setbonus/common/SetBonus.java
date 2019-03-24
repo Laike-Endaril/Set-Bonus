013059e4439bdf04ca0669b7ab131aad23274dd4
Kyle Koder <silvertallon@gmail.com>
2019-03-24 02:14:21 -0700

    Indev build 1.12.2.008a

--- src/main/java/com/fantasticsource/setbonus/common/SetBonus.java
+++ src/main/java/com/fantasticsource/setbonus/common/SetBonus.java
@@ -26,7 +26,7 @@ public class SetBonus
 {
     public static final String MODID = "setbonus";
     public static final String NAME = "Set Bonus";
-    public static final String VERSION = "1.12.2.008";
+    public static final String VERSION = "1.12.2.008a";
 
     @EventHandler
     public static void preInit(FMLPreInitializationEvent event)
