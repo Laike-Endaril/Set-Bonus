85a1db14b068b757dfe0b67bc9d13fedb65baa31
Kyle Koder <silvertallon@gmail.com>
2019-03-26 12:59:11 -0700

    This handles the full config name in a more correct manner

--- src/main/java/com/fantasticsource/setbonus/config/ConfigHandler.java
+++ src/main/java/com/fantasticsource/setbonus/config/ConfigHandler.java
@@ -1,5 +1,6 @@
 package com.fantasticsource.setbonus.config;
 
+import com.fantasticsource.mctools.MCTools;
 import com.fantasticsource.setbonus.SetBonus;
 
 import java.io.File;
@@ -7,5 +8,5 @@ import java.io.File;
 public class ConfigHandler
 {
     public static final String CONFIG_NAME = SetBonus.MODID + "/" + SetBonus.CONFIG_VERSION + "+";
-    public static final String FULL_CONFIG_NAME = new File("config/" + CONFIG_NAME).getAbsolutePath();
+    public static final String FULL_CONFIG_NAME = new File(MCTools.getConfigDir() + CONFIG_NAME).getAbsolutePath();
 }
