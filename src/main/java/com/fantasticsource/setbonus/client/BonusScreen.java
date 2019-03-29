d8422802bc735d4688e6a7943cca200a380e41fe
Kyle Koder <silvertallon@gmail.com>
2019-03-28 21:02:24 -0700

    Did some abstraction of new GUI classes; I'll be able to move them to library later

--- src/main/java/com/fantasticsource/setbonus/client/BonusScreen.java
+++ src/main/java/com/fantasticsource/setbonus/client/BonusScreen.java
@@ -1,51 +1,27 @@
-package com.fantasticsource.setbonus.client.gui;
+package com.fantasticsource.setbonus.client;
 
+import com.fantasticsource.setbonus.client.gui.GUIScreen;
 import com.fantasticsource.setbonus.client.gui.guielements.GradientBorder;
 import com.fantasticsource.setbonus.client.gui.guielements.GradientRect;
-import com.fantasticsource.setbonus.client.gui.guielements.IGUIElement;
 import com.fantasticsource.setbonus.client.gui.guielements.VerticalScrollbar;
 import com.fantasticsource.tools.datastructures.Color;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import org.lwjgl.input.Keyboard;
-
-import java.util.ArrayList;
 
 import static com.fantasticsource.setbonus.client.Keys.BONUS_SCREEN_KEY;
 
-@SideOnly(Side.CLIENT)
-public class BonusScreen extends GuiScreen
+public class BonusScreen extends GUIScreen
 {
     private static final Color BLANK = new Color(0), BLACK = new Color(0xCC), AQUA = new Color(0x3366CC), WHITE = new Color(0xFFFFFF33), WHITE_2 = new Color(0xFFFFFF77), WHITE_3 = new Color(0xFFFFFFAA);
+    private static BonusScreen bonusScreen = new BonusScreen();
     private static long debounce = System.currentTimeMillis();
-    private static ArrayList<IGUIElement> guiElements = new ArrayList<>();
-
-
-    static
-    {
-        init();
-    }
-
+    public ItemStack stack = null;
 
     public BonusScreen()
-    {
-        Minecraft.getMinecraft().displayGuiScreen(this);
-    }
-
-    public BonusScreen(ItemStack stack)
-    {
-        Minecraft.getMinecraft().displayGuiScreen(this);
-    }
-
-
-    public static void init()
     {
         //Background
         guiElements.add(new GradientRect(0, 0, 1, 1, BLACK, BLACK, AQUA, AQUA));
@@ -70,7 +46,6 @@ public class BonusScreen extends GuiScreen
         guiElements.add(new VerticalScrollbar(59d / 60, 0, 1, 1, WHITE_2, BLANK, WHITE_2, BLANK));
     }
 
-
     @SubscribeEvent
     public static void tooltip(RenderTooltipEvent.PostText event)
     {
@@ -81,7 +56,8 @@ public class BonusScreen extends GuiScreen
             if (time - debounce > 200)
             {
                 debounce = time;
-                new BonusScreen(stack);
+                bonusScreen.stack = stack;
+                Minecraft.getMinecraft().displayGuiScreen(bonusScreen);
             }
         }
     }
@@ -91,30 +67,8 @@ public class BonusScreen extends GuiScreen
     {
         if (BONUS_SCREEN_KEY.isPressed() && BONUS_SCREEN_KEY.getKeyConflictContext().isActive())
         {
-            new BonusScreen();
+            bonusScreen.stack = null;
+            Minecraft.getMinecraft().displayGuiScreen(bonusScreen);
         }
-    }
-
-
-    @Override
-    public void drawScreen(int mouseX, int mouseY, float partialTicks)
-    {
-        GlStateManager.disableTexture2D();
-        GlStateManager.enableBlend();
-        GlStateManager.disableAlpha();
-        GlStateManager.shadeModel(7425);
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-
-        GlStateManager.pushMatrix();
-        GlStateManager.scale(width, height, 1);
-
-        for (IGUIElement element : guiElements) element.draw(width, height);
-
-        GlStateManager.popMatrix();
-
-        GlStateManager.shadeModel(7424);
-        GlStateManager.disableBlend();
-        GlStateManager.enableAlpha();
-        GlStateManager.enableTexture2D();
     }
 }
---
+++ src/main/java/com/fantasticsource/setbonus/client/gui/GUIScreen.java
@@ -0,0 +1,41 @@
+package com.fantasticsource.setbonus.client.gui;
+
+import com.fantasticsource.setbonus.client.gui.guielements.IGUIElement;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.ArrayList;
+
+@SideOnly(Side.CLIENT)
+public abstract class GUIScreen extends GuiScreen
+{
+    protected ArrayList<IGUIElement> guiElements = new ArrayList<>();
+
+    public GUIScreen()
+    {
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks)
+    {
+        GlStateManager.disableTexture2D();
+        GlStateManager.enableBlend();
+        GlStateManager.disableAlpha();
+        GlStateManager.shadeModel(7425);
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+
+        GlStateManager.pushMatrix();
+        GlStateManager.scale(width, height, 1);
+
+        for (IGUIElement element : guiElements) element.draw(width, height);
+
+        GlStateManager.popMatrix();
+
+        GlStateManager.shadeModel(7424);
+        GlStateManager.disableBlend();
+        GlStateManager.enableAlpha();
+        GlStateManager.enableTexture2D();
+    }
+}
