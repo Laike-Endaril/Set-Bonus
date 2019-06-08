30b9825fcb6fdc443747d0726f05a990a37830b8
Kyle Koder <silvertallon@gmail.com>
2019-06-08 15:14:22 -0700

    1.12.2.10

--- src/main/java/com/fantasticsource/setbonus/SetBonus.java
+++ src/main/java/com/fantasticsource/setbonus/SetBonus.java
@@ -39,7 +39,7 @@ public class SetBonus
 {
     public static final String MODID = "setbonus";
     public static final String NAME = "Set Bonus";
-    public static final String VERSION = "1.12.2.009";
+    public static final String VERSION = "1.12.2.010";
     public static final String CONFIG_VERSION = "1.12.2.009";
 
 
