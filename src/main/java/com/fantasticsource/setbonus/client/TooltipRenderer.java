1b4a56fae0e741436fd4788519493bc87615b07b
Kyle Koder <silvertallon@gmail.com>
2019-06-09 00:53:52 -0700

    Re-added the active/required set item counts on each bonus tooltip

--- src/main/java/com/fantasticsource/setbonus/client/TooltipRenderer.java
+++ src/main/java/com/fantasticsource/setbonus/client/TooltipRenderer.java
@@ -66,17 +66,17 @@ public class TooltipRenderer
                             ClientBonus.BonusInstance bonusInstance = bonus.getBonusInstance(player);
 
                             color = "";
+                            int active = set.getNumberEquipped(player);
+
                             if (bonusInstance.active) color += GREEN; //All requirements met
                             else
                             {
-                                int active = set.getNumberEquipped(player);
-
                                 if (active >= req) color += DARK_PURPLE; //Set requirements are met, but non-set requirements are not met
                                 else if (active == 0) color += RED; //No set requirements met
                                 else color += YELLOW; //Some set requirements met
                             }
 
-                            tooltip.add(color + " " + bonus.name + (otherReqs ? "*" : ""));
+                            tooltip.add(color + " " + bonus.name + " (" + active + "/" + req + ")" + (otherReqs ? "*" : ""));
                         }
                     }
                     tooltip.add("");
