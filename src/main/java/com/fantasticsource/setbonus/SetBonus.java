d704703265a917239ab981f30aee3af90b489639
Kyle Koder <silvertallon@gmail.com>
2019-12-26 01:25:58 -0800

    Updated lib and req

--- src/main/java/com/fantasticsource/setbonus/SetBonus.java
+++ src/main/java/com/fantasticsource/setbonus/SetBonus.java
@@ -37,7 +37,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import static net.minecraftforge.fml.common.Mod.EventHandler;
 
-@Mod(modid = SetBonus.MODID, name = SetBonus.NAME, version = SetBonus.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.026,)")
+@Mod(modid = SetBonus.MODID, name = SetBonus.NAME, version = SetBonus.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.029b,)")
 public class SetBonus
 {
     public static final String MODID = "setbonus";
