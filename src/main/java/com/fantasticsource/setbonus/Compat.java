6fd5fc7be72cb7ea6426e16bbc749915b462b7b8
Laike-Endaril <silvertallon@gmail.com>
2025-04-21 06:29:26 -0700

    For #25, superior JEI compat performance, much better than JEI's normal performance, even

--- src/main/java/com/fantasticsource/setbonus/Compat.java
+++ src/main/java/com/fantasticsource/setbonus/Compat.java
@@ -3,9 +3,13 @@ package com.fantasticsource.setbonus;
 import com.fantasticsource.tools.ReflectionTool;
 import it.unimi.dsi.fastutil.chars.Char2ObjectMap;
 import mezz.jei.Internal;
+import mezz.jei.gui.ingredients.IIngredientListElement;
+import mezz.jei.ingredients.AdaptiveIngredientFilterBackgroundBuilder;
 import mezz.jei.ingredients.IngredientFilter;
+import mezz.jei.ingredients.IngredientFilterBackgroundBuilder;
 import mezz.jei.ingredients.TooltipSearchTree;
 import mezz.jei.suffixtree.CombinedSearchTrees;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 
@@ -30,6 +34,19 @@ public class Compat
         CombinedSearchTrees combinedSearchTrees = (CombinedSearchTrees) ReflectionTool.invoke(IngredientFilter.class, "buildCombinedSearchTrees", ingredientFilter, searchTree, prefixedSearchTrees.values());
         ReflectionTool.set(IngredientFilter.class, "combinedSearchTrees", ingredientFilter, combinedSearchTrees);
         ReflectionTool.set(IngredientFilter.class, "filterCached", ingredientFilter, null);
-        MinecraftForge.EVENT_BUS.register(ReflectionTool.get(IngredientFilter.class, "backgroundBuilder", ingredientFilter));
+
+        //The part with lots of computation (per item count in modpack)
+        //JEI already does this in a deferred, limited CPU-time-per-tick way, but I've improved upon their method...a LOT
+        //Theirs simply used 40ms per client tick (20ms, 2 times per tick, once at START phase and once at END phase)
+        //Mine limits CPU usage to END phase only, based on how much CPU time is still available to work within that tick without going below 20tps
+        IngredientFilterBackgroundBuilder backgroundBuilder = (IngredientFilterBackgroundBuilder) ReflectionTool.get(IngredientFilter.class, "backgroundBuilder", ingredientFilter);
+        if (backgroundBuilder instanceof AdaptiveIngredientFilterBackgroundBuilder) ((AdaptiveIngredientFilterBackgroundBuilder) backgroundBuilder).buildOrRebuild();
+        else
+        {
+            MinecraftForge.EVENT_BUS.unregister(backgroundBuilder);
+            NonNullList<IIngredientListElement> elementList = (NonNullList<IIngredientListElement>) ReflectionTool.get(IngredientFilterBackgroundBuilder.class, "elementList", backgroundBuilder);
+            backgroundBuilder = new AdaptiveIngredientFilterBackgroundBuilder(prefixedSearchTrees.values(), elementList);
+            ReflectionTool.set(IngredientFilter.class, "backgroundBuilder", ingredientFilter, backgroundBuilder);
+        }
     }
 }
---
+++ src/main/java/mezz/jei/ingredients/AdaptiveIngredientFilterBackgroundBuilder.java
@@ -0,0 +1,84 @@
+package mezz.jei.ingredients;
+
+import mezz.jei.config.Config;
+import mezz.jei.gui.ingredients.IIngredientListElement;
+import mezz.jei.suffixtree.GeneralizedSuffixTree;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import java.util.Collection;
+
+public class AdaptiveIngredientFilterBackgroundBuilder extends IngredientFilterBackgroundBuilder
+{
+    //This class only extends IngredientFilterBackgroundBuilder for compat with original JEI when reflecting into it
+    //If this was ever added directly to JEI code, it could simply be the new version of the existing IngredientFilterBackgroundBuilder instead, and the start() method override could be removed
+    //It could also use Collection<PrefixedSearchTree> instead of a generic Collection as an argument, and no cast in the constructor (I only did this because PrefixedSearchTree is non-public)
+    protected final Collection<PrefixedSearchTree> prefixedSearchTrees;
+    protected final NonNullList<IIngredientListElement> elementList;
+    protected long lastStepTime = 0;
+
+    public AdaptiveIngredientFilterBackgroundBuilder(Collection prefixedSearchTrees, NonNullList<IIngredientListElement> elementList)
+    {
+        super(null, null);
+        this.prefixedSearchTrees = (Collection<PrefixedSearchTree>) prefixedSearchTrees;
+        this.elementList = elementList;
+        buildOrRebuild();
+    }
+
+    @Override
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onClientTick(TickEvent.ClientTickEvent event)
+    {
+        if (Minecraft.getMinecraft().player != null && event.phase == TickEvent.Phase.END)
+        {
+            if (buildOrRebuild())
+            {
+                lastStepTime = 0;
+                MinecraftForge.EVENT_BUS.unregister(this);
+            }
+        }
+    }
+
+    @Override
+    public void start()
+    {
+        buildOrRebuild();
+    }
+
+    public boolean buildOrRebuild()
+    {
+        if (lastStepTime == 0)
+        {
+            lastStepTime = System.currentTimeMillis() + 50;
+            MinecraftForge.EVENT_BUS.register(this);
+        }
+
+
+        final long stopTime = lastStepTime + 50;
+        for (PrefixedSearchTree prefixedTree : this.prefixedSearchTrees)
+        {
+            Config.SearchMode mode = prefixedTree.getMode();
+            if (mode != Config.SearchMode.DISABLED)
+            {
+                PrefixedSearchTree.IStringsGetter stringsGetter = prefixedTree.getStringsGetter();
+                GeneralizedSuffixTree tree = prefixedTree.getTree();
+                for (int i = tree.getHighestIndex() + 1; i < this.elementList.size(); i++)
+                {
+                    IIngredientListElement element = elementList.get(i);
+                    Collection<String> strings = stringsGetter.getStrings(element);
+
+                    if (strings.isEmpty()) tree.put("", i);
+                    else for (String string : strings) tree.put(string, i);
+
+                    if (System.currentTimeMillis() >= stopTime) return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
