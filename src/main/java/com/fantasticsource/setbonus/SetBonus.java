ea2f036af0b5442bd8a500c5f8a0af67ebf155df
Laike-Endaril <silvertallon@gmail.com>
2025-04-22 14:33:13 -0700

    1.12.2.031

--- src/main/java/com/fantasticsource/setbonus/SetBonus.java
+++ src/main/java/com/fantasticsource/setbonus/SetBonus.java
@@ -42,7 +42,7 @@ public class SetBonus
 {
     public static final String MODID = "setbonus";
     public static final String NAME = "Set Bonus";
-    public static final String VERSION = "1.12.2.030";
+    public static final String VERSION = "1.12.2.031";
     public static final String CONFIG_VERSION = "1.12.2.009";
 
 
