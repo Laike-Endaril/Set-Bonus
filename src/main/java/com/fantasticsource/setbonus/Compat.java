6e23c0df775b40779769303dee480bf53c6c8989
Laike-Endaril <silvertallon@gmail.com>
2025-04-23 19:33:23 -0700

    For #29, Threaded JEI tooltip reload

--- src/main/java/com/fantasticsource/setbonus/Compat.java
+++ src/main/java/com/fantasticsource/setbonus/Compat.java
@@ -5,15 +5,12 @@ import it.unimi.dsi.fastutil.chars.Char2ObjectMap;
 import mezz.jei.Internal;
 import mezz.jei.config.Config;
 import mezz.jei.gui.ingredients.IIngredientListElement;
-import mezz.jei.ingredients.AdaptiveIngredientFilterBackgroundBuilder;
 import mezz.jei.ingredients.IngredientFilter;
-import mezz.jei.ingredients.IngredientFilterBackgroundBuilder;
-import mezz.jei.ingredients.TooltipSearchTree;
+import mezz.jei.ingredients.ThreadedIngredientFilterBackgroundBuilder;
 import mezz.jei.search.AdaptiveSearchable;
 import mezz.jei.search.CombinedSearchables;
 import mezz.jei.search.PrefixInfo;
 import mezz.jei.search.PrefixedSearchable;
-import mezz.jei.suffixtree.CombinedSearchTrees;
 import net.minecraft.util.NonNullList;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
@@ -55,29 +52,13 @@ public class Compat
     public static void refreshJEITooltips()
     {
         IngredientFilter ingredientFilter = Internal.getIngredientFilter();
-        Char2ObjectMap prefixedSearchTrees = (Char2ObjectMap) ReflectionTool.get(IngredientFilter.class, "prefixedSearchTrees", ingredientFilter);
-        TooltipSearchTree tooltipSearchTree = new TooltipSearchTree();
-        prefixedSearchTrees.put('#', tooltipSearchTree);
-
-        //Based on IngredientFilter.modesChanged()
-        Object searchTree = ReflectionTool.get(IngredientFilter.class, "searchTree", ingredientFilter);
-        CombinedSearchTrees combinedSearchTrees = (CombinedSearchTrees) ReflectionTool.invoke(IngredientFilter.class, "buildCombinedSearchTrees", ingredientFilter, searchTree, prefixedSearchTrees.values());
-        ReflectionTool.set(IngredientFilter.class, "combinedSearchTrees", ingredientFilter, combinedSearchTrees);
-        ReflectionTool.set(IngredientFilter.class, "filterCached", ingredientFilter, null);
-
-        //The part with lots of computation (per item count in modpack)
-        //JEI already does this in a deferred, limited CPU-time-per-tick way, but I've improved upon their method...a LOT
-        //Theirs simply used 40ms per client tick (20ms, 2 times per tick, once at START phase and once at END phase)
-        //Mine limits CPU usage to END phase only, based on how much CPU time is still available to work within that tick without going below 20tps
         Object backgroundBuilder = ReflectionTool.get(IngredientFilter.class, "backgroundBuilder", ingredientFilter);
-        if (backgroundBuilder instanceof AdaptiveIngredientFilterBackgroundBuilder) ((AdaptiveIngredientFilterBackgroundBuilder) backgroundBuilder).buildOrRebuild();
-        else
-        {
-            MinecraftForge.EVENT_BUS.unregister(backgroundBuilder);
-            NonNullList<IIngredientListElement> elementList = (NonNullList<IIngredientListElement>) ReflectionTool.get(IngredientFilterBackgroundBuilder.class, "elementList", backgroundBuilder);
-            backgroundBuilder = new AdaptiveIngredientFilterBackgroundBuilder(prefixedSearchTrees.values(), elementList);
-            ReflectionTool.set(IngredientFilter.class, "backgroundBuilder", ingredientFilter, backgroundBuilder);
-        }
+        Char2ObjectMap prefixedSearchTrees = (Char2ObjectMap) ReflectionTool.get(IngredientFilter.class, "prefixedSearchTrees", ingredientFilter);
+
+        MinecraftForge.EVENT_BUS.unregister(backgroundBuilder);
+        NonNullList<IIngredientListElement> elementList = (NonNullList<IIngredientListElement>) ReflectionTool.get(IngredientFilter.class, "elementList", ingredientFilter);
+        backgroundBuilder = new ThreadedIngredientFilterBackgroundBuilder(prefixedSearchTrees.values(), elementList);
+        ReflectionTool.set(IngredientFilter.class, "backgroundBuilder", ingredientFilter, backgroundBuilder);
     }
 
     public static void refreshHEITooltips()
---
+++ src/main/java/mezz/jei/ingredients/ThreadedIngredientFilterBackgroundBuilder.java
@@ -0,0 +1,134 @@
+package mezz.jei.ingredients;
+
+import com.fantasticsource.setbonus.Compat;
+import com.fantasticsource.tools.ReflectionTool;
+import mezz.jei.Internal;
+import mezz.jei.config.Config;
+import mezz.jei.gui.ingredients.IIngredientListElement;
+import mezz.jei.suffixtree.CombinedSearchTrees;
+import mezz.jei.suffixtree.GeneralizedSuffixTree;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ThreadedIngredientFilterBackgroundBuilder extends IngredientFilterBackgroundBuilder
+{
+    protected static volatile ThreadedData QUEUED_DATA = null, PROCESSED_DATA = null;
+    protected static Thread thread = null;
+    protected static final Runnable runnable = () ->
+    {
+        ThreadedData workingData;
+
+        while (QUEUED_DATA != null)
+        {
+            workingData = QUEUED_DATA;
+            QUEUED_DATA = null;
+
+
+            Collection<String> strings;
+            for (Map.Entry<PrefixedSearchTree.IStringsGetter, GeneralizedSuffixTree> entry : workingData.dataMap.entrySet())
+            {
+                GeneralizedSuffixTree tree = entry.getValue();
+                for (int i = 0; i < workingData.elements.length; i++)
+                {
+                    strings = entry.getKey().getStrings(workingData.elements[i]);
+
+                    if (strings.isEmpty()) tree.put("", i);
+                    else for (String string : strings) tree.put(string, i);
+
+
+                    if (QUEUED_DATA != null) break;
+                }
+                if (QUEUED_DATA != null) break;
+            }
+
+
+            if (QUEUED_DATA == null) PROCESSED_DATA = workingData;
+        }
+    };
+
+    static
+    {
+        MinecraftForge.EVENT_BUS.register(ThreadedIngredientFilterBackgroundBuilder.class);
+    }
+
+
+    protected final Collection<PrefixedSearchTree> trees;
+    protected final int elementCount;
+
+    public ThreadedIngredientFilterBackgroundBuilder(Collection prefixedSearchTrees, NonNullList<IIngredientListElement> elementList)
+    {
+        super(null, null);
+        trees = prefixedSearchTrees;
+        elementCount = elementList.size();
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        QUEUED_DATA = new ThreadedData(prefixedSearchTrees, elementList);
+        if (thread != null && thread.isAlive()) thread.stop();
+        thread = new Thread(runnable);
+        thread.setName("Set Bonus JEI Tooltip Reload");
+        thread.start();
+    }
+
+
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public void clientTick(TickEvent.ClientTickEvent event)
+    {
+        if (event.phase == TickEvent.Phase.END && PROCESSED_DATA != null)
+        {
+            ThreadedData data = PROCESSED_DATA;
+            PROCESSED_DATA = null;
+
+            if (data.elements.length == elementCount)
+            {
+                for (PrefixedSearchTree tree : trees)
+                {
+                    ReflectionTool.set(PrefixedSearchTree.class, "tree", tree, data.dataMap.get(tree.getStringsGetter()));
+                }
+
+                IngredientFilter ingredientFilter = Internal.getIngredientFilter();
+                Object searchTree = ReflectionTool.get(IngredientFilter.class, "searchTree", ingredientFilter);
+                CombinedSearchTrees combinedSearchTrees = (CombinedSearchTrees) ReflectionTool.invoke(IngredientFilter.class, "buildCombinedSearchTrees", ingredientFilter, searchTree, trees);
+                ReflectionTool.set(IngredientFilter.class, "combinedSearchTrees", ingredientFilter, combinedSearchTrees);
+                ReflectionTool.set(IngredientFilter.class, "filterCached", ingredientFilter, null);
+                Internal.getRuntime().getIngredientListOverlay().updateLayout(true);
+            }
+        }
+    }
+
+    @Override
+    public void start()
+    {
+        Compat.refreshJEITooltips();
+    }
+
+    @Override
+    public void onClientTick(TickEvent.ClientTickEvent event)
+    {
+    }
+
+
+    public static class ThreadedData
+    {
+        LinkedHashMap<PrefixedSearchTree.IStringsGetter, GeneralizedSuffixTree> dataMap = new LinkedHashMap<>();
+        IIngredientListElement[] elements;
+
+        ThreadedData(Collection<PrefixedSearchTree> prefixedSearchTrees, NonNullList<IIngredientListElement> elementList)
+        {
+            for (PrefixedSearchTree tree : prefixedSearchTrees)
+            {
+                if (tree.getMode() != Config.SearchMode.DISABLED) dataMap.put(tree.getStringsGetter(), new GeneralizedSuffixTree());
+            }
+            this.elements = elementList.toArray(new IIngredientListElement[0]);
+        }
+    }
+}
