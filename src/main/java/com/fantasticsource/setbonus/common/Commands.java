36af077482db208272b9018d79af1a06e48daf0f
Laike-Endaril <silvertallon@gmail.com>
2025-05-25 10:54:44 -0700

    Code cleanup

--- src/main/java/com/fantasticsource/setbonus/common/Commands.java
+++ src/main/java/com/fantasticsource/setbonus/common/Commands.java
@@ -6,11 +6,9 @@ import com.fantasticsource.setbonus.SetBonusData;
 import com.fantasticsource.setbonus.config.ConfigHandler;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -63,11 +61,6 @@ public class Commands extends CommandBase
             {
                 MCTools.reloadConfig(ConfigHandler.FULL_CONFIG_NAME + ".cfg", SetBonus.MODID);
                 SetBonusData.setServerFromConfig();
-
-                for (EntityPlayerMP player : FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayers())
-                {
-                    Network.updateConfig(player);
-                }
                 notifyCommandListener(sender, this, SetBonus.MODID + ".cmd.reloaded");
             }
             catch (IllegalAccessException e)
