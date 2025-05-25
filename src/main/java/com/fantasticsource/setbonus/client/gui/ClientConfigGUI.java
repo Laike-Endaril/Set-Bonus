5fea73bcec46d0797c264ccaa05db6d9c30efa56
Laike-Endaril <silvertallon@gmail.com>
2025-05-24 23:04:45 -0700

    Don't show JEI / HEI refresh config in GUI if tooltips are disabled

--- src/main/java/com/fantasticsource/setbonus/client/gui/ClientConfigGUI.java
+++ src/main/java/com/fantasticsource/setbonus/client/gui/ClientConfigGUI.java
@@ -26,34 +26,37 @@ public class ClientConfigGUI extends GUIScreen
         root.add(new GUIDarkenedBackground(this));
         GUIElement element = new GUINavbar(this);
         root.add(element);
-
-
-        root.add(new GUITextSpacer(this));
         root.add(new GUITextSpacer(this));
 
 
         //Main
+        GUIBooleanToggle tooltips = new GUIBooleanToggle(this, reformat(MODID + ".config.enableTooltips")).set(SetBonusConfig.clientSettings.enableTooltips);
+        root.add(tooltips);
+        root.add(new GUITextSpacer(this));
+
+
+        GUIBooleanToggle attributeModTooltips = new GUIBooleanToggle(this, reformat(MODID + ".config.enableAttributeModifierTooltips")).set(SetBonusConfig.clientSettings.enableAttributeModifierTooltips);
+        GUIBooleanToggle potionTooltips = new GUIBooleanToggle(this, reformat(MODID + ".config.enablePotionEffectTooltips")).set(SetBonusConfig.clientSettings.enablePotionEffectTooltips);
+        GUIBooleanToggle enchantmentTooltips = new GUIBooleanToggle(this, reformat(MODID + ".config.enableEnchantmentTooltips")).set(SetBonusConfig.clientSettings.enableEnchantmentTooltips);
+        GUITextSpacer spacer = new GUITextSpacer(this);
+
+
         GUIStringPicker dynamicTooltipSearch = new GUIStringPicker(this, reformat(MODID + ".config.dynamicTooltipSearch"),
                 reformat(MODID + ".config.dynamicTooltipSearch.never"),
                 reformat(MODID + ".config.dynamicTooltipSearch.onBonusDiscovery"),
                 reformat(MODID + ".config.dynamicTooltipSearch.always")
         );
         dynamicTooltipSearch.set(dynamicTooltipSearch.possibleValues[SetBonusConfig.clientSettings.dynamicTooltipSearch]);
-        root.add(dynamicTooltipSearch);
 
-
-        GUIBooleanToggle tooltips = new GUIBooleanToggle(this, reformat(MODID + ".config.enableTooltips")).set(SetBonusConfig.clientSettings.enableTooltips);
-        root.add(tooltips);
-
-        GUIBooleanToggle attributeModTooltips = new GUIBooleanToggle(this, reformat(MODID + ".config.enableAttributeModifierTooltips")).set(SetBonusConfig.clientSettings.enableAttributeModifierTooltips);
-        GUIBooleanToggle potionTooltips = new GUIBooleanToggle(this, reformat(MODID + ".config.enablePotionEffectTooltips")).set(SetBonusConfig.clientSettings.enablePotionEffectTooltips);
-        GUIBooleanToggle enchantmentTooltips = new GUIBooleanToggle(this, reformat(MODID + ".config.enableEnchantmentTooltips")).set(SetBonusConfig.clientSettings.enableEnchantmentTooltips);
 
         if (tooltips.value)
         {
             root.add(attributeModTooltips);
             root.add(potionTooltips);
             root.add(enchantmentTooltips);
+            root.add(spacer);
+
+            root.add(dynamicTooltipSearch);
         }
 
 
@@ -72,6 +75,9 @@ public class ClientConfigGUI extends GUIScreen
                 root.add(attributeModTooltips);
                 root.add(potionTooltips);
                 root.add(enchantmentTooltips);
+                root.add(spacer);
+
+                root.add(dynamicTooltipSearch);
             }
             else
             {
@@ -79,6 +85,9 @@ public class ClientConfigGUI extends GUIScreen
                 root.remove(attributeModTooltips);
                 root.remove(potionTooltips);
                 root.remove(enchantmentTooltips);
+                root.remove(spacer);
+
+                root.remove(dynamicTooltipSearch);
             }
             MCTools.saveConfig(MODID);
         });
